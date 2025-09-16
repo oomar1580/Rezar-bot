0045318cde14de4e3baf4911e024eb72270e51c4
monkybama <tsabyhshms@gmail.com>
2025-09-16 20:33:17 +0200

    Add files via upload

--- includes/handle/handleCreateDatabase.js
+++ includes/handle/handleCreateDatabase.js
@@ -1,120 +1,69 @@
+
 module.exports = function ({ Users, Threads, Currencies }) {
-    const logger = require(process.cwd() + "/utils/log.js");
-
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
-
+    const logger =require("../../utils/log.js");
     return async function ({ event }) {
-        const { allUserID, allCurrenciesID, allThreadID, userName, threadInfo } = global.data;
+        const { allUserID, allCurrenciesID, allThreadID, userName, threadInfo } = global.data; 
         const { autoCreateDB } = global.config;
-        if (!autoCreateDB) return;
-
-        let { senderID, threadID } = event;
+        if (autoCreateDB == ![]) return;
+        var { senderID, threadID } = event;
         senderID = String(senderID);
-        threadID = String(threadID);
-
+        var threadID = String(threadID);
         try {
-            const threadExists = await Threads.getData(threadID);
-
-            if (event.isGroup && !threadExists) {
+            if (!allThreadID.includes(threadID) && event.isGroup == !![]) {
                 const threadIn4 = await Threads.getInfo(threadID);
-                const dataThread = {
-                    threadID: threadIn4.threadID,
-                    threadName: threadIn4.threadName,
-                    participantIDs: threadIn4.participantIDs,
-                    userInfo: threadIn4.userInfo,
-                    unreadCount: 0,
-                    messageCount: 0,
-                    timestamp: Date.now().toString(),
-                    muteUntil: null,
-                    isGroup: threadIn4.isGroup,
-                    isSubscribed: true,
-                    isArchived: false,
-                    folder: "INBOX",
-                    cannotReplyReason: null,
-                    eventReminders: [],
-                    emoji: threadIn4.emoji,
-                    color: threadIn4.color,
-                    threadTheme: {
-                        id: threadIn4?.threadTheme?.id || "",
-                        accessibility_label: threadIn4?.threadTheme?.accessibility_label || ""
-                    },
-                    nicknames: threadIn4.nicknames,
-                    adminIDs: threadIn4.adminIDs,
-                    approvalMode: threadIn4.approvalMode,
-                    approvalQueue: [],
-                    reactionsMuteMode: "reactions_not_muted",
-                    mentionsMuteMode: "mentions_not_muted",
-                    isPinProtected: false,
-                    relatedPageThread: null,
-                    snippet: "",
-                    snippetSender: "",
-                    snippetAttachments: [],
-                    serverTimestamp: Date.now().toString(),
-                    imageSrc: threadIn4.imageSrc || "",
-                    isCanonicalUser: false,
-                    isCanonical: false,
-                    recipientsLoadable: true,
-                    hasEmailParticipant: false,
-                    readOnly: false,
-                    canReply: true,
-                    lastMessageType: "message",
-                    lastReadTimestamp: Date.now().toString(),
-                    threadType: 2,
-                    inviteLink: threadIn4.inviteLink
-                };
-
-                allThreadID.push(threadID);
+                const setting = {};
+                setting.threadName = threadIn4.threadName
+                setting.adminIDs = threadIn4.adminIDs
+                setting.nicknames = threadIn4.nicknames;
+                const dataThread = setting;
+                allThreadID.push(threadID)
                 threadInfo.set(threadID, dataThread);
-
-                await Threads.setData(threadID, { threadInfo: dataThread, data: {} });
-
-                for (const singleData of threadIn4.userInfo) {
-                    if (singleData.gender !== undefined) {
-                        userName.set(String(singleData.id), singleData.name);
-                        if (!allUserID.includes(String(singleData.id)) || !userName.has(String(singleData.id)) || !(await Users.getData(singleData.id))) {
-                            await Users.createData(singleData.id, {
-                                name: singleData.name,
-                                gender: singleData.gender
-                            });
-                            allUserID.push(String(singleData.id));
-                            logger(`New user: ${singleData.id} | ${singleData.name}`, '[ DATABASE ] >');
-                            await sleep(500);
-                        }
-                    }
+                var job = ["FF9900","FFFF33","33FFFF","FF99FF","FF3366","FFFF66","FF00FF","66FF99","00CCFF","FF0099","FF0066","008E97","F58220","38B6FF","7ED957","97FFFF","00BFFF","76EEC6","4EEE94","98F5FF","AFD788","00B2BF","9F79EE","00FA9A"];
+                const chalk = require('chalk');
+                var random = job[Math.floor(Math.random() * job.length)]      
+        var random1 = job[Math.floor(Math.random() * job.length)]
+        var random2 = job[Math.floor(Math.random() * job.length)]
+                const setting2 = {};
+                setting2.threadInfo = dataThread
+                setting2.data = {}
+                await Threads.setData(threadID, setting2);
+                for (singleData of threadIn4.userInfo) {
+                    userName.set(String(singleData.id), singleData.name);
+                    try {
+                        global.data.allUserID.includes(String(singleData.id)) ? (await Users.setData(String(singleData.id), 
+                        {
+                            'name': singleData.name
+                        }), 
+                        global.data.allUserID.push(singleData.id)) : (await Users.createData(singleData.id, 
+                        {
+                            'name': singleData.name,
+                            'data': {}
+                        }), 
+                        global.data.allUserID.push(String(singleData.id)), 
+                        global.data.allUserID.push(String(singleData.name)), 
+                        logger(global.getText('handleCreateDatabase', 'newUser', chalk.hex("#" + random)(`New user:  `) + chalk.hex("#" + random1)(`${singleData.name}`) + "  ||  " + chalk.hex("#" + random2)(`${singleData.id}`)), '[ USER ]'));
+                    } catch(e) { console.log(e) };
                 }
-
-                logger(`Nhóm mới: ${threadID} | ${threadIn4.threadName}`, '[ DATABASE ] >');
+                logger(global.getText('handleCreateDatabase', 'newThread',chalk.hex("#" + random)(`New group: `) + chalk.hex("#" + random1)(`${threadID}`) + "  ||  " + chalk.hex("#" + random2)(`${threadIn4.threadName}`)), '[ THREAD ]');
             }
-
-            const userExists = await Users.getData(senderID);
-
-            if (!userExists) {
-                const infoUsers = await Users.getInfo(senderID);
-                const setting3 = {
-                    name: infoUsers.name,
-                    gender: infoUsers.gender
-                };
-                await Users.createData(senderID, setting3);
-                allUserID.push(senderID);
-                userName.set(senderID, infoUsers.name);
-                logger(`Người dùng mới: ${senderID} | ${infoUsers.name}`, '[ DATABASE ] >');
-                await sleep(500);
+            if (!allUserID.includes(senderID) || !userName.has(senderID)) {
+                const infoUsers = await Users.getInfo(senderID),
+                    setting3 = {};
+                setting3.name = infoUsers.name
+                await Users.createData(senderID, setting3)
+                allUserID.push(senderID) 
+                userName.set(senderID, infoUsers.name)
+                logger(global.getText('handleCreateDatabase', 'newUser', chalk.hex("#" + random)(`New users: `) + chalk.hex("#" + random1)(`${singleData.name}`) + " || " + chalk.hex("#" + random2)(`${senderID}`)), '[ USER ]');
             }
-
             if (!allCurrenciesID.includes(senderID)) {
-                const setting4 = {
-                    data: {}
-                };
-                await Currencies.createData(senderID, setting4);
+                const setting4 = {};
+                setting4.data = {}
+                await Currencies.createData(senderID, setting4) 
                 allCurrenciesID.push(senderID);
-                await sleep(500);
             }
-
             return;
         } catch (err) {
-            console.log(err);
+            return console.log(err);
         }
     };
-};
+}
